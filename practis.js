0e6a89307cd662948edd69ef27c94ba346cc51a7
junaidsabri1317 <xhan.junaid@gmail.com>
2023-09-20 11:02:32 +0500

    arr tuple

--- practis.js
+++ practis.js
@@ -1,3 +1,9 @@
 "use strict";
 var result = "junaid Sabri";
 console.log(result);
+let arr = ["asif", 4, false,];
+arr.push("js");
+arr.push("sj");
+arr.push(0, 4);
+arr.push({ name: "sercar" });
+console.log(arr);
--- practis.ts
+++ practis.ts
@@ -1,4 +1,10 @@
 var result:string = "junaid Sabri"
     console.log(result);
 
-    
+    let arr:[string,number,boolean,{name:string}?]=["asif",4,false,]
+
+    arr.push("js")
+    arr.push("sj")
+    arr.push(0,4)
+    arr.push({name:"sercar"})
+    console.log(arr);
