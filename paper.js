cc8e9eb234f84a309e20243abd4da389386cc684
junaidsabri1317 <xhan.junaid@gmail.com>
2023-10-04 09:34:58 +0500

    Message

---
+++ paper.js
@@ -0,0 +1,7 @@
+"use strict";
+function funName() {
+    let total = 2 + 2;
+    return total;
+}
+let name1 = funName();
+console.log(name1);
---
+++ paper.ts
@@ -0,0 +1,7 @@
+function funName(){
+    let total
+    = 2+2
+    return total
+}
+let name1:number= funName();
+console.log(name1);
