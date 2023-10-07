8ca1047db29a5ae5c6b3394700c1b0870f013ade
junaidsabri1317 <xhan.junaid@gmail.com>
2023-10-07 10:17:20 +0500

    Message

---
+++ reduce.js
@@ -0,0 +1,14 @@
+"use strict";
+let balance = [2, 4, 6, 8, 1];
+// let data = balance.reduce((total,elem)=>{
+// return total+elem},44)
+// let data = balance.reduce((total,elem)=>{
+//     return total-elem},50)
+// let data = balance.reduce((total,elem)=>{
+//     return total*elem},10)
+//let data = balance.reduce((total,elem)=>{
+//return total-elem},10)
+let data1 = balance.reduce((total, elem) => {
+    return total = elem;
+}, 20);
+console.log("data", data1);
---
+++ reduce.ts
@@ -0,0 +1,15 @@
+let balance:number[]=[2,4,6,8,1];
+// let data = balance.reduce((total,elem)=>{
+// return total+elem},44)
+// let data = balance.reduce((total,elem)=>{
+//     return total-elem},50)
+    // let data = balance.reduce((total,elem)=>{
+    //     return total*elem},10)
+        
+    //let data = balance.reduce((total,elem)=>{
+        //return total-elem},10)
+        let data1 = balance.reduce((total,elem)=>{
+            return total=elem},20)
+        
+    
+console.log("data",data1);
