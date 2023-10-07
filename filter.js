68923c858fc5b4ad8302e4b7ac7304da77c183b9
junaidsabri1317 <xhan.junaid@gmail.com>
2023-10-07 12:03:22 +0500

    Message

--- filter.js
+++ filter.js
@@ -1,7 +1,18 @@
 "use strict";
-let c = [1, 2, 3, 4, 5];
-let newc = c.filter((elem) => {
-    return elem > 1 && elem < 4;
+// let c=[1,2,3,4,5];
+// let newc = c.filter((elem)=>{
+//     return elem > 1 && elem<4   ;
+// });
+// console.log(c);
+// console.log(newc);
+let nam = [{ name: "a", age: 1, desig: "jco" },
+    { name: "b", age: 2, desig: "lfr" },
+    { name: "c", age: 3, desig: "sc" },
+    { name: "d", age: 4, desig: "EMT" },
+    { name: "e", age: 5, desig: "SI" },
+];
+let data = nam.find((elem) => {
+    return elem.name == "d";
 });
-console.log(c);
-console.log(newc);
+console.log(data);
+console.log(data === null || data === void 0 ? void 0 : data.age);
--- filter.ts
+++ filter.ts
@@ -1,6 +1,26 @@
-let c=[1,2,3,4,5];
-let newc = c.filter((elem)=>{
-    return elem > 1 && elem<4   ;
-});
-console.log(c);
-console.log(newc);
+// let c=[1,2,3,4,5];
+// let newc = c.filter((elem)=>{
+//     return elem > 1 && elem<4   ;
+// });
+// console.log(c);
+// console.log(newc);
+
+
+
+type jTypes={
+    name: string,
+    age: number,
+    desig: string};
+  
+  let nam:jTypes[] = 
+  [  { name: "a", age: 1, desig: "jco" },
+    { name: "b", age: 2, desig: "lfr" },
+    { name: "c", age: 3, desig: "sc" },
+    { name: "d", age: 4, desig: "EMT" },
+    { name: "e", age: 5, desig: "SI" },
+  ]
+  let data= nam.find((elem) => {
+    return elem.name == "d"
+  });
+  console.log(data);
+  console.log(data?.age);
